c70f5bc741bd04e9116fd34a56c64a6397759af1
zhiriezra <zhiriezra@gmail.com>
2025-03-15 20:57:29 +0100

    hello

--- src/pages/gallerys/index.tsx
+++ src/pages/gallerys/index.tsx
@@ -12,8 +12,8 @@ export default function Gallery() {
             Coming Soon
           </h1>
           <p className="text-xl text-muted-foreground">
-            We're working hard to bring you something amazing. Stay tuned for
-            updates.
+            We&apos;re working hard to bring you something amazing. Stay tuned
+            for updates.
           </p>
         </div>
       </main>
